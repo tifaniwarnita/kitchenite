f00fced5a435274fe1d5e6360057612533332b5b
William Sentosa <williamsentosa.sentosa@gmail.com>
2016-08-11 15:59:13 +0700

    added models

---
+++ Back end/api-kitchenite/app/Ingredient_nutrient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient_nutrient extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ingredient_nutrients';
+}
