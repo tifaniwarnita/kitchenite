558994a99e0541a7f685721f19761cb26e3dee25
Erick Chandra <erickchandra_1@yahoo.com>
2016-08-31 09:04:36 +0700

    Added mock up Insert New Recipe (new activity).

---
+++ app/src/main/java/com/gencat/kitchenite/AddNewRecipeActivity.java
@@ -0,0 +1,13 @@
+package com.gencat.kitchenite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_recipe);
+    }
+}
